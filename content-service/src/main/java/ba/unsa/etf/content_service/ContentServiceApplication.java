16d4de5376e7374ce5f9aeb31f892e2f731ac3d3
Hamza Cepić <hcepic1@etf.unsa.ba>
2025-03-27 02:02:13 +0100

    Opis promjena

--- content-service/src/main/java/ba/unsa/etf/content_service/ContentServiceApplication.java
+++ content-service/src/main/java/ba/unsa/etf/content_service/ContentServiceApplication.java
@@ -2,6 +2,8 @@ package ba.unsa.etf.content_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ContentServiceApplication {
@@ -9,5 +11,4 @@ public class ContentServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ContentServiceApplication.class, args);
 	}
-
 }
