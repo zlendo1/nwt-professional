f59a837831b8ea783ec9ab0b6c046edf025a36ff
zlendo1 <zlendo1@etf.unsa.ba>
2025-05-07 14:33:26 +0200

    updated CreateMessageDTO and added UpdateMessageDTO

--- communication-service/src/main/java/ba/unsa/etf/communication_service/dto/message/UpdateMessageDTO.java
+++ communication-service/src/main/java/ba/unsa/etf/communication_service/dto/message/UpdateMessageDTO.java
@@ -1,7 +1,6 @@
 package ba.unsa.etf.communication_service.dto.message;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,13 +8,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class CreateMessageDTO {
-  @NotNull(message = "User id canot be null")
-  private Long userId;
-
-  @NotNull(message = "Conversation id cannot be null")
-  private Long conversationId;
-
+public class UpdateMessageDTO {
   @NotBlank(message = "Content cannot be blank")
   private String content;
 }
