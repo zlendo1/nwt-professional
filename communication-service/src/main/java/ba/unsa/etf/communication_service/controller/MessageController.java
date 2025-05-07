4f6752b531e85eff916704371611c8767284f5f5
zlendo1 <zlendo1@etf.unsa.ba>
2025-05-07 14:34:06 +0200

    added Message service and controller

---
+++ communication-service/src/main/java/ba/unsa/etf/communication_service/controller/MessageController.java
@@ -0,0 +1,65 @@
+package ba.unsa.etf.communication_service.controller;
+
+import ba.unsa.etf.communication_service.dto.message.CreateMessageDTO;
+import ba.unsa.etf.communication_service.dto.message.MessageDTO;
+import ba.unsa.etf.communication_service.service.MessageService;
+import jakarta.validation.Valid;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/message")
+@AllArgsConstructor
+public class MessageController {
+  private final MessageService messageService;
+
+  @GetMapping
+  public ResponseEntity<List<MessageDTO>> getAll() {
+    return ResponseEntity.ok(messageService.findAll());
+  }
+
+  @GetMapping(params = {"id"})
+  public ResponseEntity<MessageDTO> getById(@RequestParam(required = true) Long id) {
+    return messageService
+        .findById(id)
+        .map(ResponseEntity::ok)
+        .orElse(ResponseEntity.notFound().build());
+  }
+
+  @GetMapping(params = {"conversationId"})
+  public List<MessageDTO> getByConversationId(@RequestParam(required = true) Long conversationId) {
+    return messageService.findByConversationId(conversationId);
+  }
+
+  @GetMapping(params = {"userId", "conversationId"})
+  public List<MessageDTO> getByUserAndConversationId(
+      @RequestParam(required = true) Long userId,
+      @RequestParam(required = true) Long conversationId) {
+    return messageService.findByUserAndConversationId(userId, conversationId);
+  }
+
+  @PostMapping
+  public ResponseEntity<MessageDTO> create(@Valid @RequestBody CreateMessageDTO dto) {
+    return ResponseEntity.ok(messageService.create(dto));
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Boolean> delete(@PathVariable Long id) {
+    boolean deleted = messageService.delete(id);
+
+    if (deleted) {
+      return ResponseEntity.noContent().build();
+    } else {
+      return ResponseEntity.notFound().build();
+    }
+  }
+}
