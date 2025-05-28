b5cba876e196599b879430f28f0ba494360a3f3e
Benjo <benkad47@gmail.com>
2025-05-28 23:52:23 +0200

    added start of right bar

--- frontend/src/components/rightbar/Rightbar.jsx
+++ frontend/src/components/rightbar/Rightbar.jsx
@@ -1,7 +1,21 @@
 import "./rightbar.css"
+import { WorkHistory } from "@mui/icons-material"
 
 export default function Rightbar() {
   return (
-    <div className="rightbar">Rightbar</div>
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        <ul className="rightbarList">
+          <li className="rightbarListItem">
+            <WorkHistory className="rightbarIcon"/>
+            Aplikacija 1
+          </li>
+          <li className="rightbarListItem">
+            <WorkHistory className="rightbarIcon"/>
+            Aplikacija 2
+          </li>
+        </ul>
+      </div>
+    </div>
   )
 }
