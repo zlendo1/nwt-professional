c892f7894868d62c10d4bdf57e033ee2f796b45d
zlendo1 <zlendo1@etf.unsa.ba>
2025-06-01 01:23:51 +0200

    added pageable capabilities to repository

--- communication-service/src/main/java/ba/unsa/etf/communication_service/repository/MessageRepository.java
+++ communication-service/src/main/java/ba/unsa/etf/communication_service/repository/MessageRepository.java
@@ -1,7 +1,8 @@
 package ba.unsa.etf.communication_service.repository;
 
 import ba.unsa.etf.communication_service.entity.Message;
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,7 +10,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
   @EntityGraph(attributePaths = {"user"})
-  List<Message> findByConversation_Id(Long conversationId);
+  Page<Message> findByConversation_Id(Long conversationId, Pageable pageable);
 
-  List<Message> findByUser_IdAndConversation_Id(Long userId, Long conversationId);
+  Page<Message> findByUser_IdAndConversation_Id(
+      Long userId, Long conversationId, Pageable pageable);
 }
